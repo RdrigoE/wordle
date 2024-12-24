78822bd542192292d278885a277bbadaa2ca93f8
Rodrigo Eusébio <rodrigojeusebio@gmail.com>
2024-12-24 12:23:11 +0000

    alterar texto

--- resources/views/livewire/game.blade.php
+++ resources/views/livewire/game.blade.php
@@ -14,21 +14,21 @@
                 }
            "
 >
-    <header class="flex justify-between items-center p-4 border-b-2 border-gray-300 relative">
-        <div class="dark:text-white">
+    <header class="text-center p-4 border-b-2 border-gray-300 relative">
+        {{-- <div class="dark:text-white">
             <svg x-data @click="$dispatch('display-help')" xmlns="http://www.w3.org/2000/svg" fill="none"
                  viewBox="0 0 24 24" stroke-width="1.5"
                  stroke="currentColor" class="w-6 h-6 hover:cursor-pointer">
                 <path stroke-linecap="round" stroke-linejoin="round"
                       d="M9.879 7.519c1.171-1.025 3.071-1.025 4.242 0 1.172 1.025 1.172 2.687 0 3.712-.203.179-.43.326-.67.442-.745.361-1.45.999-1.45 1.827v.75M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9 5.25h.008v.008H12v-.008z"/>
             </svg>
-        </div>
+        </div> --}}
 
-        <div class="text-4xl font-bold">
+        <div class="text-xl font-bold text-center">
             <h1 class="text-white">FELIZ NATAL COM O TERMO</h1>
         </div>
 
-        <div class="flex space-x-2 dark:text-white">
+        {{-- <div class="flex space-x-2 dark:text-white">
             <svg x-data @click="$dispatch('display-stats')" xmlns="http://www.w3.org/2000/svg" fill="none"
                  viewBox="0 0 24 24" stroke-width="1.5"
                  stroke="currentColor" class="w-6 h-6 hover:cursor-pointer">
@@ -42,7 +42,7 @@
                       d="M9.594 3.94c.09-.542.56-.94 1.11-.94h2.593c.55 0 1.02.398 1.11.94l.213 1.281c.063.374.313.686.645.87.074.04.147.083.22.127.324.196.72.257 1.075.124l1.217-.456a1.125 1.125 0 011.37.49l1.296 2.247a1.125 1.125 0 01-.26 1.431l-1.003.827c-.293.24-.438.613-.431.992a6.759 6.759 0 010 .255c-.007.378.138.75.43.99l1.005.828c.424.35.534.954.26 1.43l-1.298 2.247a1.125 1.125 0 01-1.369.491l-1.217-.456c-.355-.133-.75-.072-1.076.124a6.57 6.57 0 01-.22.128c-.331.183-.581.495-.644.869l-.213 1.28c-.09.543-.56.941-1.11.941h-2.594c-.55 0-1.02-.398-1.11-.94l-.213-1.281c-.062-.374-.312-.686-.644-.87a6.52 6.52 0 01-.22-.127c-.325-.196-.72-.257-1.076-.124l-1.217.456a1.125 1.125 0 01-1.369-.49l-1.297-2.247a1.125 1.125 0 01.26-1.431l1.004-.827c.292-.24.437-.613.43-.992a6.932 6.932 0 010-.255c.007-.378-.138-.75-.43-.99l-1.004-.828a1.125 1.125 0 01-.26-1.43l1.297-2.247a1.125 1.125 0 011.37-.491l1.216.456c.356.133.751.072 1.076-.124.072-.044.146-.087.22-.128.332-.183.582-.495.644-.869l.214-1.281z"/>
                 <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"/>
             </svg>
-        </div>
+        </div> --}}
     </header>
 
     <div class="w-full min-h-[calc(100vh-75px)] flex justify-center items-center @if($status !== GameStatus::ACTIVE) absolute top-0 left-0 @else hidden @endif">
@@ -65,16 +65,19 @@
                  "shadow-[0_20px_50px_rgba(8,_112,_184,_0.7)]",
                  "invisible" => $status === GameStatus::ACTIVE,
            ])>
-            <span class="text-xl flex justify-center items-center">
+            <span class="text-xl flex flex-col justify-center items-center text-center">
                 @if($status === GameStatus::LOST)
-                    <span class="text-2xl">😭</span> <span>Nunca pensei que falhasses mas a palavra é</span> <span
-                        class="font-bold uppercase ml-1">{{$word}}</span>
+                    <span class="text-2xl">😭</span> Não conseguiste agarrar esta pista...<p> Abre a próxima caixa para mais pistas! Vais adorar!!!<p>
                 @elseif($status === GameStatus::WON)
-                <span class="text-2xl ">🎄</span> Boa amor, podes abrir a próxima caixa <span class="text-2xl "> 🎄</span>
+                <span class="text-2xl ">🎄</span> <span>Parabéns! Podes abrir a próxima caixa para saberes mais sobre a tua prenda </span>
                 @endif
-            </span>
+
             <a href="#" class="font-medium underline hover:no-underline cursor-pointer ml-1"
                wire:click="replay">replay</a>
+            </span>
+            <p>
+
+            </p>
         </div>
     </div>
 
